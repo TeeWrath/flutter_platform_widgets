0c224f3895c275a5095f0fd849e8dc23068db08f
Subroto <subroto.2003@gmail.com>
2025-01-24 12:52:21 +0530

    platform drop down button started

---
+++ lib/src/platform_dropdown_button.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
+
+class PlatformDropdownButton extends StatelessWidget {
+  const PlatformDropdownButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton(items: [], onChanged: (value) {});
+  }
+}
